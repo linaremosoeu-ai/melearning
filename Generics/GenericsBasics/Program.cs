315786c221abd56aa400f7d273b94b028fea396a
linaremosoeu-ai <linaremosoeu@gmail.com>
2026-05-04 16:17:04 +0000

    melearning progress

---
+++ Generics/GenericsBasics/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+//
+
+/*
+ string [] names = {"John","Jane","Doe","Pule","Nkele"};
+
+Console.WriteLine("Using foreach loop:");
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
+Console.WriteLine("\nUsing for loop:");
+for (int i = 0; i < names.Length; i++)
+{
+    Console.WriteLine(names[i]);
+} */
+//
+
+
+
+// Generic List
+List<string> names = new List<string> {"John","Jane","Doe","Pule","Nkele"};
